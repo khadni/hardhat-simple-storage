2c60d3d0579989f30336e1d1ea5ad1bf70fe0ce7
Karim <98668332+khadni@users.noreply.github.com>
2022-06-27 21:23:37 +0200

    commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@ require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 require("@nomiclabs/hardhat-etherscan");
 require("hardhat-gas-reporter");
+require("solidity-coverage");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
